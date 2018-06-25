1dcf4f0591a3f3ab93261a3d4c6a45d2ad675165
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-25 17:58:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! I BUILT THIS! I built it and i'm awesome."
+    "Welcome to your app!!!! I BUILT THIS! I built it and i'm awesome. i also have a dog."
   end
 
 end
