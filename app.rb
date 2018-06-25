53572b7528bce680484b37262c1a427d7fa2c4fa
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-25 17:59:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! I BUILT THIS! I built it and i'm awesome. i also have a dog."
   end
 
 end
